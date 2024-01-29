64d4a5917a04b559c01b7d1812f38288c3112616
Alexander <kerroline86@gmail.com>
2024-01-29 16:39:43 +0500

    fix: published provider

--- src/Providers/PhpGoExcelServiceProvider.php
+++ src/Providers/PhpGoExcelServiceProvider.php
@@ -9,12 +9,12 @@ class PhpGoExcelServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../../config/config.php' => config_path('php_go_excel.php'),
-            //   __DIR__ . '/translations/' => resource_path(),
-        ]);
-
         if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../../config/config.php' => config_path('php-go-excel.php'),
+                //   __DIR__ . '/translations/' => resource_path(),
+            ]);
+
             $this->commands([
                 SetupCommand::class,
             ]);
