e3a04ff70490dd8b9800e9103759d5315ec969e0
Александр Синягин <sinyagin.aleksandr@lab-company.ru>
2024-04-15 17:09:00 +0500

    fix: ошибка новичка

--- src/Writer.php
+++ src/Writer.php
@@ -6,25 +6,4 @@ use Kerroline\PhpGoExcel\Entities\Internals\BaseWriter;
 
 final class Writer extends BaseWriter
 {
-    protected function getGeneratorCommandPath(): string
-    {
-        /** 
-         * TODO: Обдумать сокращение архитектуры до 32/64
-         * Как сюда врезать винду
-         */
-        $commandPath = '';
-
-        $os = php_uname('s');
-        $arch = php_uname('m');
-        $osTemplate = "{$os}/{$arch}";
-
-        $commandPath = dirname(__DIR__) .  "/bin/{$osTemplate}/generator";
-
-        return $commandPath;
-    }
-
-    protected function getDataFilePath(): string
-    {
-        return dirname(__DIR__) . '/data/json_data.json';
-    }
 }
